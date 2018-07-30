836fff6292e4aa0eaf3d01682954e2787475ebb5
Daniel Ma <danyowma@gmail.com>
2018-07-29 20:28:54 -0700

    Clean up Budgets component

--- src/components/Budgets.vue
+++ src/components/Budgets.vue
@@ -1,31 +1,14 @@
 <template>
   <div>
     <h5>Select A Budget</h5>
-    <div v-for="budget in budgets" class="row" v-bind:key="budget.id">
-      <a class="col" href="#" @click="selectBudget(budget.id, $event)">{{budget.name}}</a>
-      <!--<span @click="selectBudget(budget.id, $event)">{{budget.name}}</span>-->
+    <div v-for="budget in budgets" :key="budget.id">
+      <a href="#" @click="selectBudget(budget.id, $event)">{{budget.name}}</a>
     </div>
   </div>
 </template>
 
 <script>
-import {
-  format,
-  startOfYear,
-  startOfMonth,
-  endOfMonth,
-  addMonths,
-  isWithinRange,
-  endOfYear
-} from "date-fns";
-
 export default {
-  props: ["budgets", "selectBudget"],
-  methods: {},
-  computed: {
-    today: function() {
-      return new Date();
-    }
-  }
+  props: ["budgets", "selectBudget"]
 };
 </script>
