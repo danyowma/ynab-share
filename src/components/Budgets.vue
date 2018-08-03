86a54da51a6fa3e76f35f9312b2b2818ec9291c7
Daniel Ma <danyowma@gmail.com>
2018-08-02 23:03:35 -0700

    Style budgets

--- src/components/Budgets.vue
+++ src/components/Budgets.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <h5>Select A Budget</h5>
-    <div v-for="budget in budgets" :key="budget.id">
-      <a href="#" @click="selectBudget(budget.id, $event)">{{budget.name}}</a>
+  <div class="container">
+    <h3 class="heading">My Budgets</h3>
+    <div class="budgets">
+      <div v-for="budget in budgets" :key="budget.id" @click="selectBudget(budget.id, $event)" class="budget">
+          <div class="budget-name">{{budget.name}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,3 +14,38 @@ export default {
   props: ["budgets", "selectBudget"]
 };
 </script>
+
+<style scoped>
+.container {
+  padding: 0 20px 20px;
+}
+
+.heading {
+  padding: 20px 0 20px;
+  margin: 0;
+  font-size: 20px;
+}
+
+.budgets {
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+}
+
+.budget {
+  display: block;
+  width: 200px;
+  height: 200px;
+  margin: 0 16px 16px 0;
+  border: 1px solid #dfe4e9;
+}
+
+.budget-name {
+  width: 100%;
+  height: 100%;
+  padding: 8px;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="hero">
+      <div>
+        <div class="hero-header">Let's talk budgets</div>
+        <div class="hero-description">
+          Show your friends that budgets aren't scary. Geek out over categories with other YNABers. We'll hide the amounts to make it less awkward.
+        </div>
+      </div>
+      <button @click="authorizeWithYNAB" class="button">Sign in with YNAB</button>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ["authorizeWithYNAB"]
+};
+</script>
+
+<style scoped>
+.hero {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: calc(100% - 64px);
+  padding: 54px 20px;
+  background: #85c3e9;
+  color: white;
+  text-align: center;
+}
+
+.hero-header {
+  padding: 0 24px 24px;
+  font: 900 60px Helvetica, Arial, sans-serif;
+  text-transform: capitalize;
+  line-height: 1em;
+}
+
+.hero-description {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 75%;
+  margin: 0 auto;
+  padding: 0 24px 24px;
+  font: 400 24px Helvetica, Arial, sans-serif;
+  line-height: 1.5em;
+}
+
+.button {
+  width: 220px;
+  padding: 24px;
+  border: none;
+  border-bottom: 5px solid transparent;
+  border-radius: 5px;
+  background: #f3783c;
+  font: 400 18px Helvetica, Arial, sans-serif;
+  color: white;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+
+.button:hover {
+  border-bottom: 5px solid #d55522;
+}
+
+.button:focus {
+  outline: none;
+}
+</style>
