0595171faca87f23339693604fabccd6781330c6
Daniel Ma <danyowma@gmail.com>
2018-08-14 22:13:41 -0700

    Style share button

--- src/components/SharedBudget.vue
+++ src/components/SharedBudget.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <button @click="clearSharedBudget">Share your own budget!</button>
+    <button @click="clearSharedBudget" class="share-button">Share your own budget!</button>
     <div v-for="categoryGroup in budget" v-bind:key="categoryGroup.name">
       <div class="category-group">
         <span class="category-group-name">{{categoryGroup.name}}</span>
@@ -59,5 +59,26 @@ export default {
 .category-group-percentage,
 .category-percentage {
   flex: 1;
+}
+
+.share-button {
+  padding: 8px 20px;
+  margin: 20px;
+  border: none;
+  border-radius: 20px;
+  background: #009cc2;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.share-button:focus {
+  outline: none;
+}
+
+@media (min-width: 500px) {
+  .share-button {
+    margin: 20px 20px 20px 0;
+  }
 }
 </style>
